efda2e00deb3a5a966b10c255a3fcf3811c96d8e
TedaLIEz <aliezted@gmail.com>
2025-02-09 21:59:34 +0800

    feat: use lang_chain to read log

--- src/agent.py
+++ src/agent.py
@@ -1,6 +1,12 @@
 from langchain_openai import AzureChatOpenAI
+from langchain.prompts import ChatPromptTemplate 
 import os
 from dotenv import load_dotenv
+from log_reader import read_log_file
+from typing import List  
+from pydantic import BaseModel, Field 
+from langchain_core.utils.function_calling import convert_pydantic_to_openai_function
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser  
 
 load_dotenv()
 
@@ -10,12 +16,19 @@ api_version = os.getenv('AZURE_OPENAI_VERSION')
 deployment_name = os.getenv('AZURE_OPENAI_DEPLOYMENT')
 
 
+class Tagging(BaseModel):
+    """Tagging text with a label."""
+    status: str = Field(description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout'.")
+    error_info: str = Field(description="The error information if the status is 'failure'.")
+
 class Agent:
     def __init__(self):
-        self.__chat = AzureChatOpenAI(temperature=0.0, api_key=api_key,  \
-                                    api_version=api_version, \
-                                    azure_deployment=deployment_name, \
-                                    azure_endpoint=endpoint)
+        self.__tagging = convert_pydantic_to_openai_function(Tagging)
+        self.__binding_functions = [self.__tagging]
+        self.__model = AzureChatOpenAI(temperature=0.0, api_key=api_key,  \
+                                       max_tokens=100,
+                                       deployment_name=deployment_name, api_version=api_version,  \
+                                    azure_endpoint=endpoint).bind(functions=self.__binding_functions, function_call={"name": "Tagging"})
 
     def respond(self, message):
         from langchain_core.messages import HumanMessage, SystemMessage
@@ -24,9 +37,14 @@ class Agent:
             HumanMessage(message),
         ]
 
-        return self.__chat.invoke(messages)
+        return self.__model.invoke(messages)
 
+    def check_log(self, file_path):
+        content = read_log_file(file_path)
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are a log analysis assistant. Please tag the log file."),
+            ("user", "{input}")
+        ])
+        tagging_chain = prompt | self.__model | JsonOutputFunctionsParser()
+        return tagging_chain.invoke({"input": content})
 
-if __name__ == '__main__':
-    agent = Agent()
-    print(agent.respond('Hello, how are you?'))
