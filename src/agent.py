24d8162ec78308337a05c6cb4024d9721bde36d0
TedaLIEz <aliezted@gmail.com>
2025-02-11 21:21:02 +0800

    feat: refactor code

--- src/agent.py
+++ src/agent.py
@@ -15,7 +15,7 @@ model = os.getenv('MODEL')
 
 class Tagging(BaseModel):
     """Tagging text with a label."""
-    status: str = Field(description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout'.")
+    status: str = Field(description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout', or 'unknown' if user input is not a log.")
     error_info: str = Field(description="The error information if the status is 'failure'. Please provide the error message in English.")
 
 class Agent:
@@ -25,26 +25,18 @@ class Agent:
                               api_base=endpoint, \
                               model=model).bind_tools(self.__binding_functions)
 
-    def respond(self, message):
-        from langchain_core.messages import HumanMessage, SystemMessage
-        messages = [
-            SystemMessage("You are helpful but sassy assistant."),
-            HumanMessage(message),
-        ]
-
-        return self.__model.invoke(messages)
-
-    def check_log(self, file_path):
-        content = read_log_file(file_path)
+    def analysis(self, message):
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are a log analysis assistant. Please tag the log file."),
+            ("system", "You are a log analysis assistant. Please analysis the log."),
             ("user", "{input}")
         ])
         tagging_chain = prompt | self.__model | PydanticToolsParser(tools=self.__binding_functions)
-        return tagging_chain.invoke({"input": content})
+        return tagging_chain.invoke({"input": message})
+
+    def check_log(self, file_path):
+        content = read_log_file(file_path)
+        return self.analysis(content)
 
 
 if __name__ == "__main__":
     agent = Agent()
-    file_path = os.path.join(os.getcwd(), "data", "logs", "0_hello-world-job.txt")
-    print(agent.respond("Hi"))
