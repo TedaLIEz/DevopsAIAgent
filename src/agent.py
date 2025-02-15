17da564946c7ab6abd06b3d763695cb5cdbfc12b
TedaLIEz <aliezted@gmail.com>
2025-02-15 11:33:48 +0800

    feat: add pylint & autopep8

--- src/agent.py
+++ src/agent.py
@@ -1,10 +1,10 @@
 import argparse
-from langchain.prompts import ChatPromptTemplate 
+from langchain.prompts import ChatPromptTemplate
 from langchain_deepseek import ChatDeepSeek
 import os
 from dotenv import load_dotenv
 from log_reader import read_log_file
-from pydantic import BaseModel, Field 
+from pydantic import BaseModel, Field
 from langchain_core.output_parsers.openai_tools import PydanticToolsParser
 
 load_dotenv()
@@ -16,22 +16,26 @@ model = os.getenv('MODEL')
 
 class Tagging(BaseModel):
     """Tagging text with a label."""
-    status: str = Field(description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout', or 'unknown' if user input is not a log.")
-    error_info: str = Field(description="The error information if the status is 'failure'. Please provide the error message in English.")
+    status: str = Field(
+        description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout', or 'unknown' if user input is not a log.")
+    error_info: str = Field(
+        description="The error information if the status is 'failure'. Please provide the error message in English.")
+
 
 class Agent:
     def __init__(self):
         self.__binding_functions = [Tagging]
-        self.__model = ChatDeepSeek(temperature=0.0, api_key=api_key,  \
-                              api_base=endpoint, \
-                              model=model).bind_tools(self.__binding_functions)
+        self.__model = ChatDeepSeek(temperature=0.0, api_key=api_key,
+                                    api_base=endpoint,
+                                    model=model).bind_tools(self.__binding_functions)
 
     def analysis(self, message):
         prompt = ChatPromptTemplate.from_messages([
             ("system", "You are a log analysis assistant. Please analysis the log."),
             ("user", "{input}")
         ])
-        tagging_chain = prompt | self.__model | PydanticToolsParser(tools=self.__binding_functions)
+        tagging_chain = prompt | self.__model | PydanticToolsParser(
+            tools=self.__binding_functions)
         return tagging_chain.invoke({"input": message})
 
     def check_log(self, file_path):
--- src/main.py
+++ src/main.py
@@ -47,7 +47,8 @@ async def read_root(request: Request):
         conclusion = payload['workflow_run']['conclusion']
         workflow_run_id = int(payload['workflow_run']['id'])
         action = payload['action']
-        logger.info(f"Workflow {workflow_run_id}, status: {action}, conclusion: {conclusion}")
+        logger.info(
+            f"Workflow {workflow_run_id}, status: {action}, conclusion: {conclusion}")
         if action != 'completed':
             return "Workflow to be run"
         if conclusion == 'cancelled':
@@ -55,11 +56,11 @@ async def read_root(request: Request):
         owner = payload['repository']['owner']['login']
         repo_name = payload['repository']['name']
         token = git_integration.get_access_token(
-                git_integration.get_installation(owner, repo_name).id
-                ).token
+            git_integration.get_installation(owner, repo_name).id
+        ).token
         git_connection = Github(
             login_or_token=token
-            )
+        )
         repo = git_connection.get_repo(f"{owner}/{repo_name}")
         workflow_run = repo.get_workflow_run(workflow_run_id)
         headers = {
@@ -68,26 +69,28 @@ async def read_root(request: Request):
         }
         response = requests.get(workflow_run.logs_url, headers=headers)
         if response.status_code == 200:
-            zip_file_path = os.path.join(os.getcwd(), 'tmp', f'{owner}/{repo_name}', 
+            zip_file_path = os.path.join(os.getcwd(), 'tmp', f'{owner}/{repo_name}',
                                          str(workflow_run_id), 'log.zip')
             return download_log_file(response.content, zip_file_path)
         else:
-            logger.error(f"Failed to fetch logs, status code: {response.status_code}")
+            logger.error(
+                f"Failed to fetch logs, status code: {response.status_code}")
             return {"error": f"Failed to fetch logs, status code: {response.status_code}"}
     return "ok"
+
 
 def download_log_file(content, zip_file_path):
     os.makedirs(os.path.dirname(zip_file_path), exist_ok=True)
     log_folder_path = os.path.dirname(zip_file_path)
     with open(zip_file_path, 'wb') as zip_file:
         zip_file.write(content)
-    
+
     # Unzip the file
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(log_folder_path)
-    
+
     os.remove(zip_file_path)
-    
+
     return {
         "message": "Logs saved and extracted successfully",
         "logs_path": log_folder_path
