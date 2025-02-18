c6258be88c353a670e451f643bac905255a7b014
TedaLIEz <aliezted@gmail.com>
2025-02-18 10:54:09 +0800

    feat: analysis log files as RAG

--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,17 @@
 import argparse
-from langchain.prompts import ChatPromptTemplate
-from langchain_deepseek import ChatDeepSeek
 import os
-from dotenv import load_dotenv
-from log_reader import read_log_file
 from pydantic import BaseModel, Field
+from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_deepseek import ChatDeepSeek
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import AzureOpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -17,9 +23,9 @@ model = os.getenv('MODEL')
 class Tagging(BaseModel):
     """Tagging text with a label."""
     status: str = Field(
-        description="The status of the tagging log. Select from 'success', 'failure', 'pending', 'timeout', or 'unknown' if user input is not a log.")
+        description="The status of the tagging log. Select from 'success', 'error', 'pending', 'timeout', or 'unknown' if user input is not a log.")
     error_info: str = Field(
-        description="The error information if the status is 'failure'. Please provide the error message in English.")
+        description="The error information if the status is 'error'. Please provide the error message in English.")
 
 
 class Agent:
@@ -28,19 +34,39 @@ class Agent:
         self.__model = ChatDeepSeek(temperature=0.0, api_key=api_key,
                                     api_base=endpoint,
                                     model=model).bind_tools(self.__binding_functions)
+        key = os.getenv('AZURE_OPENAI_API_KEY')
+        deployment_name = os.getenv('AZURE_OPENAI_EMBEDDING_MODEL')
+        self.__embeddings = AzureOpenAIEmbeddings(
+            openai_api_key=key, deployment=deployment_name, chunk_size=2048)
 
-    def analysis(self, message):
+    def __format_docs(self, docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    def check_logs(self, file_folder):
+        loader = DirectoryLoader(
+            file_folder, glob="*.txt", show_progress=True, loader_cls=TextLoader)
+        data = loader.load()
+        # Split
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500, chunk_overlap=0)
+        all_splits = text_splitter.split_documents(data)
+        # Store splits
+        vectorstore = FAISS.from_documents(
+            documents=all_splits, embedding=self.__embeddings)
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are a log analysis assistant. Please analysis the log."),
-            ("user", "{input}")
+            ("human", """You are an assistant for log analysis. Use the following pieces of retrieved context to analysis the log. 
+                Use one sentences maximum and keep the answer concise.
+                    Question: {question} 
+                    Context: {context} 
+             """)
         ])
-        tagging_chain = prompt | self.__model | PydanticToolsParser(
+        question = "Extract any error information from the log. If there is no error, please provide the status of the log."
+        tagging_chain = {
+            "context": vectorstore.as_retriever() | self.__format_docs,
+            "question": RunnablePassthrough()
+        } | prompt | self.__model | PydanticToolsParser(
             tools=self.__binding_functions)
-        return tagging_chain.invoke({"input": message})
-
-    def check_log(self, file_path):
-        content = read_log_file(file_path)
-        return self.analysis(content)
+        return tagging_chain.invoke(question)
 
 
 if __name__ == "__main__":
@@ -49,7 +75,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     agent = Agent()
     try:
-        result = agent.check_log(args.log_file)
-        print(result)
+        response = agent.check_logs(args.log_file)
+        print(response)
     except Exception as e:
         print(f"Error: {e}")
