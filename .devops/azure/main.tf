a1d990dbc446bf3d0f57e7733c675a7918a7450d
TedaLIEz <aliezted@gmail.com>
2025-02-24 20:52:21 +0800

    feat: deploy container registry & service plan

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -17,3 +17,19 @@ resource "azurerm_resource_group" "rg" {
   name     = "devopsAgentRg"
   location = "westus2"
 }
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = "devopsAgentContainerRegistry"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "devopsAgentServicePlan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "FREE"
+}
