3ebcfe33b85eb974a75e0af3b859bb5508fe3591
TedaLIEz <aliezted@gmail.com>
2025-02-25 07:52:03 +0800

    feat: try to read data from resource group

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -13,23 +13,23 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "devopsAgentRg"
-  location = "westus2"
+data "azurerm_resource_group" "rg" {
+  name = "devopsAgentRg"
 }
+
 
 
 resource "azurerm_container_registry" "acr" {
   name                = "devopsAgentContainerRegistry"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
 
 resource "azurerm_service_plan" "asp" {
   name                = "devopsAgentServicePlan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "FREE"
 }
