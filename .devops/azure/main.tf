e2d97accf62ff7d508c0247a2c54f1185d2049f7
TedaLIEz <aliezted@gmail.com>
2025-02-26 08:56:18 +0800

    feat: try to destroy container registry

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -25,13 +25,6 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-resource "azurerm_container_registry" "acr" {
-  name                = "devopsAgentContainerRegistry"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-}
-
 resource "azurerm_service_plan" "asp" {
   name                = "devopsAgentServicePlan"
   resource_group_name = azurerm_resource_group.rg.name
