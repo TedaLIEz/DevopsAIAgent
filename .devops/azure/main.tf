dca52e2e33417a29f5afe998c1a6b2007b60a850
TedaLIEz <aliezted@gmail.com>
2025-02-27 22:10:14 +0800

    fix: fix docker image tag && ENABLE CI

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -54,9 +54,13 @@ resource "azurerm_linux_web_app" "app" {
 
     application_stack {
       docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
-      docker_image_name        = "devops_agent"
+      docker_image_name        = "devops_agent:latest"
       docker_registry_password = azurerm_container_registry.acr.admin_password
       docker_registry_username = azurerm_container_registry.acr.admin_username
     }
+  }
+
+  app_settings = {
+    DOCKER_ENABLE_CI = "true"
   }
 }
