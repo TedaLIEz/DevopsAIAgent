9ba1deae532b0ce61a2aaed7eb04c43dccab8ab6
TedaLIEz <aliezted@gmail.com>
2025-02-27 19:52:28 +0800

    feat: come up with new name

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_user_assigned_identity" "identity" {
 
 
 resource "azurerm_linux_web_app" "app" {
-  name                = "devopsagent"
+  name                = "build-log-inspector"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
