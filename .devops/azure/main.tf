71c846753cdc87d559e7f9305397e8d8df8d8b55
TedaLIEz <aliezted@gmail.com>
2025-02-27 21:41:54 +0800

    feat: update act config

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -41,12 +41,6 @@ resource "azurerm_service_plan" "asp" {
   sku_name            = "F1"
 }
 
-resource "azurerm_user_assigned_identity" "identity" {
-  name                = "mi_devopsAgent"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
-
 
 resource "azurerm_linux_web_app" "app" {
   name                = "build-log-inspector"
@@ -59,10 +53,8 @@ resource "azurerm_linux_web_app" "app" {
     always_on = false
 
     application_stack {
-      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
-      docker_image_name        = "devops_agent"
-      docker_registry_username = azurerm_container_registry.acr.admin_username
-      docker_registry_password = azurerm_container_registry.acr.admin_password
+      docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
+      docker_image_name   = "devops_agent"
     }
   }
 }
