ec7293fa019ff8e043649e04ca1a1a0ca1b44d32
TedaLIEz <aliezted@gmail.com>
2025-03-27 21:22:59 +0800

    feat: update container log configuration

--- .devops/azure/variables.tf
+++ .devops/azure/variables.tf
@@ -101,3 +101,24 @@ variable "grafana_name" {
 variable "grafana_version" {
   default = "10"
 }
+
+variable "streams" {
+  default = ["Microsoft-ContainerLog", "Microsoft-ContainerLogV2", "Microsoft-KubeEvents", "Microsoft-KubePodInventory", "Microsoft-KubeNodeInventory", "Microsoft-KubePVInventory", "Microsoft-KubeServices", "Microsoft-KubeMonAgentEvents", "Microsoft-InsightsMetrics", "Microsoft-ContainerInventory", "Microsoft-ContainerNodeInventory", "Microsoft-Perf"]
+}
+
+
+variable "data_collection_interval" {
+  default = "1m"
+}
+
+variable "namespace_filtering_mode_for_data_collection" {
+  default = "Off"
+}
+
+variable "namespaces_for_data_collection" {
+  default = ["kube-system", "gatekeeper-system", "azure-arc"]
+}
+
+variable "enableContainerLogV2" {
+  default = true
+}
