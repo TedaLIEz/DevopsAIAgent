a12240bcbf31457f501cdfc7b0270676f8f44100
TedaLIEz <aliezted@gmail.com>
2025-04-09 09:02:51 +0800

    fix: fix acr

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -25,15 +25,6 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = var.acr_sku
-  admin_enabled       = true
-}
-
-
 
 resource "azurerm_log_analytics_workspace" "law" {
   name                = var.log_analytics_name
