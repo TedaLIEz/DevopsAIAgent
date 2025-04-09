7c16db8c4f2459bdddb4fdc9020375be641b516c
TedaLIEz <aliezted@gmail.com>
2025-04-09 09:23:29 +0800

    fix: try to deploy with web app

--- .devops/azure/main.tf
+++ .devops/azure/main.tf
@@ -35,3 +35,47 @@ resource "azurerm_log_analytics_workspace" "law" {
 }
 
 
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.acr_sku
+  admin_enabled       = true
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = var.app_service_sku
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.web_app_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+
+  site_config {
+    always_on = false
+
+    application_stack {
+      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
+      docker_image_name        = var.acr_image_name
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+    }
+  }
+
+  app_settings = {
+    DOCKER_ENABLE_CI = "true"
+  }
+
+
+}
+
+
+
+
